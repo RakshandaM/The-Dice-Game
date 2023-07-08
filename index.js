dfee1565e7290656f7b594bcecf10e7cf5647066
Chinmay Bhoir <bhoirchinmay2014@gmail.com>
2023-07-08 14:22:24 +0530

    Fix UI issues

--- index.js
+++ index.js
@@ -1,17 +1,30 @@
-var randomNumber1 = Math.floor(Math.random() * 6) + 1; //1-6
-var RandomImageSource = "images/dice" + randomNumber1 + ".png"; //dice1.png - dice6.png
-document.querySelector(".img1").setAttribute("src", RandomImageSource);
+// Roll the dice button
+var button = document.querySelector(".btn");
+button.addEventListener("click", start);
 
-var randomNumber2 = Math.floor(Math.random() * 6) + 1;
-var RandomImageSource2 = "images/dice" + randomNumber2 + ".png";
-document.querySelector(".img2").setAttribute("src", RandomImageSource2);
+function start() {
+  var randomNumber1 = Math.floor(Math.random() * 6) + 1; //1-6
+  var randomDiceImage = "dice" + randomNumber1 + ".png"; //dice1.png - dice6.png
+  var randomImageSource = "images/" + randomDiceImage; //images/dice1.png - images/dice6.png
+  var image1 = document.querySelectorAll("img")[0]; //first image
+  image1.setAttribute("src", randomImageSource); //set the image source to the random image
 
-//player 1
+  var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+  var randomImageSource2 = "images/dice" + randomNumber2 + ".png"; //images/dice1.png - images/dice6.png
+  var image2 = document
+    .querySelectorAll("img")[1]
+    .setAttribute("src", randomImageSource2);
 
-if (randomNumber1 > randomNumber2) {
-  document.querySelector("h1").innerHTML = "🚩 Player 1 Wins! 🚩";
-} else if (randomNumber2 > randomNumber1) {
-  document.querySelector("h1").innerHTML = "🚩 Player 2 wins! 🚩";
-} else {
-  document.querySelector("h1").innerHTML = "Draw!";
+  //player 1
+
+  if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML =
+      "<span>🚩</span> Player 1 Wins! <span>🚩</span>";
+  } else if (randomNumber2 > randomNumber1) {
+    document.querySelector("h1").innerHTML =
+      "<span>🚩</span> Player 2 wins! <span>🚩</span>";
+  } else {
+    document.querySelector("h1").innerHTML =
+      "<span>🚩</span> Draw! <span>🚩</span>";
+  }
 }
